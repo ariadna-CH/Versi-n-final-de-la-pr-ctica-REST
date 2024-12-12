99b1c833dc3539534c954ca568abe99705ca482f
Ariadna Cruz <arcruz@evaltech.com.mx>
2024-12-11 20:41:04 -0600

    Reset

--- orm/repo.py
+++ orm/repo.py
@@ -1,7 +1,7 @@
 import orm.modelos as modelos
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
-import orm.esquemas as esquemas 
+
 
 #----------------------PETICIONES ALUMNOS-----------------------------#
 #SELECT * FROM app.alumnos
@@ -15,52 +15,6 @@ def alumno_por_id(sesion:Session,id_alumno:int): #Mediante la Session se pueden 
     print("SELECT * FROM app.alumnos WHERE id=", id_alumno)
     #Que tabla quiero utilizar, y se aplica un filtro ya que solo quiero que me muestre uno y que sea por id
     return sesion.query(modelos.Alumno).filter(modelos.Alumno.id==id_alumno).first() #se devuelve un objeto de tipo alumno
-#----------------------RES2-----------------------------#
-#POST '/alumnos'
-#Insertar alumno
-def insertar_alumno(sesion:Session, alumno_nuevo:esquemas.AlumnoBase):
-    #1. Crear un nuevo objeto de la clase modelo alumno
-    alumno_bd = modelos.Alumno()
-    #2. Llenar los parámetros del nuuevo objeto con los parámetros que nos pasó el usuario
-    alumno_bd.nombre = alumno_nuevo.nombre
-    alumno_bd.edad = alumno_nuevo.edad
-    alumno_bd.domicilio = alumno_nuevo.domicilio
-    alumno_bd.carrera = alumno_nuevo.carrera
-    alumno_bd.trimestre = alumno_nuevo.trimestre
-    alumno_bd.email = alumno_nuevo.email
-    alumno_bd.password = alumno_nuevo.password
-    #3. Insertar el nuevo objeto a la BD
-    sesion.add(alumno_bd)
-    #4. Confirmamos el cambio
-    sesion.commit()
-    #5. Hacemos un refresh
-    sesion.refresh(alumno_bd)
-    return alumno_bd
-
-#PUT '/alumnos/{id}'
-#Actualizar datos de un alumno
-def actualiza_alumno(sesion:Session, id_alumno:int,alumno_actualizado:esquemas.AlumnoBase):
-    #1. Verfificar si el alumno existe
-    alumno_bd = alumno_por_id(sesion,id_alumno)
-    if alumno_bd is not None:
-        #2. Actualizamos los datos del alumno en la base de datos
-        alumno_bd.nombre = alumno_actualizado.nombre
-        alumno_bd.edad = alumno_actualizado.edad
-        alumno_bd.domicilio = alumno_actualizado.domicilio
-        alumno_bd.carrera = alumno_actualizado.carrera
-        alumno_bd.trimestre = alumno_actualizado.trimestre
-        alumno_bd.email = alumno_actualizado.email
-        alumno_bd.password = alumno_actualizado.password
-        #3. Confirmamos los cambios
-        sesion.commit()
-        #4. Reflescar la base de datos
-        sesion.refresh(alumno_bd)
-        #5. Imprimir los datos nuevos
-        print(alumno_actualizado)
-        return(alumno_actualizado)
-    else:
-        respuesta = {"mensaje":"No existe el alumno"}
-        return respuesta
     
 #----------------------PETICIONES FOTO-----------------------------#
 #SELECT * FROM app.fotos
@@ -75,29 +29,6 @@ def foto_por_id(sesion:Session,id_foto:int):
     return sesion.query(modelos.Foto).filter(modelos.Foto.id==id_foto).first() 
 
 
-#----------------------RES2-----------------------------#
-#PUT '/fotos/{id}'
-#Actualizar datos de fotos
-def actualiza_foto(sesion:Session, id_foto:int,foto_actualizada:esquemas.FotoBase):
-    #1. Verfificar si foto existe
-    foto_bd = foto_por_id(sesion,id_foto)
-    if foto_bd is not None:
-        #2. Actualizamos los datos del la foto en la base de datos
-        foto_bd.titulo = foto_actualizada.titulo
-        foto_bd.descripcion = foto_actualizada.descripcion
-        foto_bd.ruta = foto_actualizada.ruta
-        #3. Confirmamos los cambios
-        sesion.commit()
-        #4. Reflescar la base de datos
-        sesion.refresh(foto_bd)
-        #5. Imprimir los datos nuevos
-        print(foto_actualizada)
-        return(foto_actualizada)
-    else:
-        respuesta = {"mensaje":"No existe la foto"}
-        return respuesta
-    
-
 #----------------------PETICIONES CALIFICACIONES-----------------------------#
 #SELECT * FROM app.calificaciones
 #GET '/calificaciones'
@@ -110,26 +41,6 @@ def calificacion_por_id(sesion:Session,id_calificacion:int):
     print("SELECT * FROM app.calificaciones WHERE id=", id_calificacion)
     return sesion.query(modelos.Calificacion).filter(modelos.Calificacion.id==id_calificacion).first() 
 
-#----------------------RES2-----------------------------#
-#PUT '/calificaciones/{id}'
-#Actualizar datos de calificaciones
-def actualiza_calificacion(sesion:Session, id_calificacion:int,calificacion_actualizada:esquemas.CalificacionBase):
-    #1. Verfificar si calificacion existe
-    calificacion_bd = calificacion_por_id(sesion,id_calificacion)
-    if calificacion_bd is not None:
-        #2. Actualizamos los datos del la calificacion en la base de datos
-        calificacion_bd.uea = calificacion_actualizada.uea
-        calificacion_bd.calificacion = calificacion_actualizada.calificación
-        #3. Confirmamos los cambios
-        sesion.commit()
-        #4. Reflescar la base de datos
-        sesion.refresh(calificacion_bd)
-        #5. Imprimir los datos nuevos
-        print(calificacion_actualizada)
-        return(calificacion_actualizada)
-    else:
-        respuesta = {"mensaje":"No existe la calificacion"}
-        return respuesta
 
 #----------------------PETICIONES OTROS-----------------------------#
 #SELECT * FROM app.fotos WHERE id_alumno={id_al}
@@ -147,42 +58,6 @@ def calificaciones_por_id_foto(sesion:Session, id_foto:int):
 def calificaciones_por_id_alumno(sesion:Session, id_alumno:int):
     print("SELECT * FROM app.calificaciones WHERE id_alumnos= ",id_alumno)
     return sesion.query(modelos.Calificacion).filter(modelos.Calificacion.id==id_alumno).all()
-
-#----------------------RES2-----------------------------#
-#POST("/alumnos/{id}/calificaciones")
-#Insertar una nueva calificacion al alumno existente
-def insertar_cal_alumno(sesion:Session, id_alumno:int, cal_por_alumno:esquemas.CalificacionBase):
-    #1. Crear un nuevo objeto de la clase modelo calificacion
-    cal_alumno_bd = modelos.Calificacion()
-    #2. Llenar los parámetros del nuevo objeto con los parámetros que nos pasó el usuario
-    cal_alumno_bd.id_alumno = id_alumno
-    cal_alumno_bd.uea = cal_por_alumno.uea
-    cal_alumno_bd.calificacion = cal_por_alumno.calificación
-    #3. Insertar el nuevo objeto a la BD
-    sesion.add(cal_alumno_bd)
-    #4. Confirmamos el cambio
-    sesion.commit()
-    #5. Hacemos un refresh
-    sesion.refresh(cal_alumno_bd)
-    return cal_alumno_bd
-
-#POST("/alumnos/{id}/fotos")
-#Insertar una nueva foto al alumno existente
-def insertar_foto_alumno(sesion:Session, id_alumno:int, foto_por_alumno:esquemas.FotoBase):
-    #1. Crear un nuevo objeto de la clase modelo foto
-    foto_alumno_bd = modelos.Foto()
-    #2. Llenar los parámetros del nuevo objeto con los parámetros que nos pasó el usuario
-    foto_alumno_bd.id_alumno = id_alumno
-    foto_alumno_bd.titulo = foto_por_alumno.titulo
-    foto_alumno_bd.descripcion = foto_por_alumno.descripcion
-    foto_alumno_bd.ruta = foto_por_alumno.ruta
-    #3. Insertar el nuevo objeto a la BD
-    sesion.add(foto_alumno_bd)
-    #4. Confirmamos el cambio
-    sesion.commit()
-    #5. Hacemos un refresh
-    sesion.refresh(foto_alumno_bd)
-    return foto_alumno_bd
 
 
 #----------------------DELETE-----------------------------#
